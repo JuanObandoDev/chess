1881db685371381834a623ae2aeca3baa8013da6
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-02-19 17:02:17 -0500

    feat: add frontend

---
+++ frontend/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Chess</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Multiplayer simple chess project" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Chess</h1>
+      <p>Multiplayer simple chess project</p>
+    </main>
+  );
+}
