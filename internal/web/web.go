d55bc381590b5cbd2ec9e2179e686487878f5286
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-03-04 15:53:08 -0500

    refactor: web write

--- internal/web/web.go
+++ internal/web/web.go
@@ -2,18 +2,22 @@ package web
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/pkg/errors"
 )
 
 func Write(w http.ResponseWriter, data interface{}) {
-	w.Header().Add("Content-Type", "application/json")
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		StatusInternalServerError(w, errors.Wrap(err, "failed to encode payload"))
+		return
+	}
 
-	encoder := json.NewEncoder(w)
-	encoder.SetEscapeHTML(false)
-
-	err := encoder.Encode(data)
+	w.Header().Add("Content-Length", fmt.Sprint(len(bytes)))
+	fmt.Println("* ", string(bytes))
+	_, err = w.Write(bytes)
 
 	if err != nil {
 		StatusInternalServerError(w, errors.Wrap(err, "Failed to write payload"))
