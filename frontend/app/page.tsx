e83e6fbb5ca2826661a21aa1b2c694c83ca790ef
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-03-04 14:14:59 -0500

    refactor(frontend): change path ../ to @/

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,13 @@
-import styles from "../styles/page.module.css";
+import styles from "@/styles/page.module.css";
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-          <h2>Thank you for choosing our web site. Please login or signup to start to play.</h2>
+        <h2>
+          Thank you for choosing our web site. Please login or signup to start
+          to play.
+        </h2>
       </main>
     </>
   );
