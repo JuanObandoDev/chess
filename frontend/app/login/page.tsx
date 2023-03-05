55fd78b77d957ee8d73d337a514dc36b098508d7
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-03-05 11:11:15 -0500

    feat(frontend): add auth/discord

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,6 +1,6 @@
 import { API_ADDRESS } from "@/config";
 
-async function getLink(): Promise<string> {
+async function getGithubLink(): Promise<string> {
   const req = new Request(`${API_ADDRESS}/auth/github/link`, {
     mode: "cors",
     credentials: "include",
@@ -14,13 +14,29 @@ async function getLink(): Promise<string> {
   return data.url;
 }
 
+async function getDiscordLink(): Promise<string> {
+  const req = new Request(`${API_ADDRESS}/auth/discord/link`, {
+    mode: "cors",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const res = await fetch(req, { cache: "force-cache" });
+  const data = await res.json();
+  return data.url;
+}
+
 export default async function Login() {
-  const link = await getLink();
+  const githubLink = await getGithubLink();
+  const discordLink = await getDiscordLink();
 
   return (
     <>
       <main>
-        <a href={link}>Github</a>
+        <a href={githubLink}>Github</a>
+        <a href={discordLink}>Discord</a>
       </main>
     </>
   );
