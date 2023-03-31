e54490be18047a6a40e62ecc31243fd22f91d4d4
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-03-31 10:43:38 -0500

    refactor(web): Remove experimental app/ directory

--- frontend/app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return (        
-        <footer>
-          <p>Copyright © 2023 SIS</p>
-        </footer>
-    )
-}
--- frontend/config.ts
+++
@@ -1,2 +0,0 @@
-export const API_ADDRESS =
-  process.env.NEXT_PUBLIC_API_ADDRESS ?? "http://localhost:8000";
