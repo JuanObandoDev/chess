2623ad1af521435b9d572966bdd60d14a2d606fd
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-04-04 11:07:28 -0500

    feat(web): add auth/github page

---
+++ web/pages/auth/github.tsx
@@ -0,0 +1,33 @@
+import { APIError } from "@/types/Error";
+import { parseAPIError } from "@/utils/error";
+import { auth } from "@/utils/auth";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+type Props = {
+  error?: APIError;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  try {
+    const cookie = await auth("github", ctx);
+    ctx.res.setHeader("set-cookie", cookie);
+    ctx.res.writeHead(302, { Location: "/" });
+    ctx.res.end();
+    return { props: {} };
+  } catch (e) {
+    console.error(e);
+    return { props: { error: parseAPIError(e) } };
+  }
+};
+
+export default function Github({
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <>
+      <div>{error?.error}</div>
+      <div>{error?.message}</div>
+      <div>{error?.suggested}</div>
+    </>
+  );
+}
